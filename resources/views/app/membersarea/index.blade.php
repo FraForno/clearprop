d0bfcb8afc781d25d1c4f2a845e1c271b587f334
FraForno <francescoforno@gmail.com>
2023-04-12 09:04:36 +0200

    iphone support WIP

--- resources/views/app/membersarea/index.blade.php
+++ resources/views/app/membersarea/index.blade.php
@@ -50,7 +50,7 @@
 					
 					<p>
 						<strong>Video Lezioni</strong><br />
-						<a href="javascript:openmodal('{{env('APP_URL');}}/docs/fonia2023/1_Enti e Regole Aria.mkv');">1 - Enti e Regole dell'Aria</a><br />
+						<a href="javascript:openmodal('{{env('APP_URL');}}/docs/fonia2023/1_Enti_ e_Regole_Aria.mkv');">1 - Enti e Regole dell'Aria</a><br />
 						<a href="javascript:openmodal('{{env('APP_URL');}}/docs/fonia2023/3_Gli Spazi Aerei.mkv');">3 - Gli Spazi Aerei</a><br />
 						<a href="javascript:openmodal('{{env('APP_URL');}}/docs/fonia2023/4_Servizi Meteo.mkv');">4 - I Servizi Meteo</a><br />
 						<a href="javascript:openmodal('{{env('APP_URL');}}/docs/fonia2023/5_Conoscenze Radioelettriche.mkv');">5 - Conoscenze Radioelettriche</a><br />
