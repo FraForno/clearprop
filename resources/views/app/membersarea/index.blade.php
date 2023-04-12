33d5e067e49a177509f5560e0ae8e77f882e6e4f
FraForno <francescoforno@gmail.com>
2023-04-12 09:24:10 +0200

    iphone support WIP

--- resources/views/app/membersarea/index.blade.php
+++ resources/views/app/membersarea/index.blade.php
@@ -66,7 +66,7 @@
 				<div id="myModal" class="modal">
 				  <div class="modal-content">
 					<span class="close" onclick="closemodal();">&times;</span>
-					<video id="video-player" controls="true" preload="none" playsinline></video>
+					<video id="video-player" controls="true" preload="none" playsinline muted></video>
 				  </div>
 				</div>
 				
