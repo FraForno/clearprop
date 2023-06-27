3f3161f50cd980516709145c980f6080ad552ecf
FraForno <francescoforno@gmail.com>
2023-06-27 17:27:29 +0200

    verbale CD

--- resources/views/app/membersarea/index.blade.php
+++ resources/views/app/membersarea/index.blade.php
@@ -124,6 +124,7 @@
 						<a href="{{env('APP_URL');}}/docs/Statuto Club Astra.pdf" target="_blank">Statuto del Club (vecchio)</a><br />
 						<a href="{{env('APP_URL');}}/docs/VERBALE CD DEL 14 GENN 23.pdf" target="_blank">Verbale CD del 14/01/2023</a><br />
 						<a href="{{env('APP_URL');}}/docs/VERBALE 18 FEBBRAIO 23.pdf" target="_blank">Verbale CD del 18/02/2023</a><br />
+						<a href="{{env('APP_URL');}}/docs/verbale 13.05.23" target="_blank">Verbale CD del 13/05/2023</a><br />
 						<a href="{{env('APP_URL');}}/docs/Statuto 2023 Aviosuperfice Club Astra ASD.pdf" target="_blank">Statuto del Club 2023</a><br />
 					</p>
                 </div>
@@ -131,11 +132,10 @@
         </div>
     </div>
 	
-	<div class="row">
+<!--	<div class="row">
         <div class="col-12">
             <div class="card card-primary card-outline">
 			
-				<!-- Fonia -->
                 <div class="card-header d-flex p-0 border-none">
                     <h3 class="card-title p-3">
                         <strong>Andamento finanziario</strong>
@@ -146,7 +146,7 @@
 					<p><img src="{{env('APP_URL');}}/images/graf1.png" alt="Grafico 1" width="538" height="413" style="border:2px solid black"></p>
 					<p><img src="{{env('APP_URL');}}/images/graf2.png" alt="Grafico 2" width="538" height="413" style="border:2px solid black"></p>
                 </div>
-	</div>
+	</div>-->
 	@endcan
 @endsection
 
