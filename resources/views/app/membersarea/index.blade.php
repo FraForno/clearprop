94b17c660c652ede3085998074d1af26554040ec
FraForno <francescoforno@gmail.com>
2024-07-30 11:07:10 +0200

    libro soci apr 2024

--- resources/views/app/membersarea/index.blade.php
+++ resources/views/app/membersarea/index.blade.php
@@ -42,6 +42,7 @@
 						<a href="{{env('APP_URL');}}/docs/verbale 13.05.23.pdf" target="_blank">Verbale CD del 13/05/2023</a><br />
 						<a href="{{env('APP_URL');}}/docs/verbale 16 12 2023.pdf" target="_blank">Verbale Assemblea Soci del 16/12/2023</a><br />
 						<a href="{{env('APP_URL');}}/docs/VERBALE ASSEMBLEA DEI SOCI DEL 20 APR 2024.pdf" target="_blank">Verbale Assemblea Soci del 20/04/2024</a><br />
+						<a href="{{env('APP_URL');}}/docs/LibroSoci20Apr2024.pdf.pdf" target="_blank">Libro Soci approvato il 20/04/2024</a><br />
 					</p>
                 </div>
             </div>
